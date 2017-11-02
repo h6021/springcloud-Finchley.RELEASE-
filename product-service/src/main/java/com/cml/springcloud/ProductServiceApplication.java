d71557271d782dbeb9f8b23d5cc8ea7aaa9edbd6
cml <menglin.chen>
2017-11-02 09:57:22 +0800

    添加商品服务

---
+++ product-service/src/main/java/com/cml/springcloud/ProductServiceApplication.java
@@ -0,0 +1,15 @@
+package com.cml.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProductServiceApplication {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ProductServiceApplication.class).web(true).run(args);
+	}
+}
