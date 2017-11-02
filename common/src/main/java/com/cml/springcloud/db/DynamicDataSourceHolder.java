027241e1365b95014542dd61a8446bbe3123f95d
cml <menglin.chen>
2017-11-02 13:35:04 +0800

    common工程动态数据源添加

---
+++ common/src/main/java/com/cml/springcloud/db/DynamicDataSourceHolder.java
@@ -0,0 +1,24 @@
+package com.cml.springcloud.db;
+
+/**
+ * 动态数据源处理
+ * 
+ * @author cml
+ *
+ */
+public class DynamicDataSourceHolder {
+	private static ThreadLocal<String> holderDataSource = new ThreadLocal<>();
+
+	public static void setDataSource(String dataSource) {
+		holderDataSource.set(dataSource);
+	}
+
+	public static String getDataSource() {
+		return holderDataSource.get();
+	}
+
+	public static void clear() {
+		holderDataSource.remove();
+	}
+
+}
